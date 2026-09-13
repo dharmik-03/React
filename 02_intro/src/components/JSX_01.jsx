4eec5c7a4a04abc4b1dcc80e70177d80050ba41c
dharmik-rabari-03 <dharmik161616@gmail.com>
2026-09-13 18:58:17 +0530

    intro code added

--- 02_intro/src/components/JSX_01.jsx
+++ 02_intro/src/components/JSX_01.jsx
@@ -1,1 +1,23 @@
 import React from "react";
+
+const JSX = () => {
+
+    //without JSX
+
+    const withOutJSX = React.createElement("h1", null, "without JSX")
+
+
+    //with JSX
+
+
+    const withJSX = <h1>with JSX</h1>;
+
+    return (
+        <>
+            <h1>{withOutJSX} {withJSX}</h1>
+
+        </>
+    )
+}
+
+export default JSX
