cd7fffbf219bef6da977bb5d1fc5220ffd67ff23
dharmik-rabari-03 <dharmik161616@gmail.com>
2026-09-15 10:23:53 +0530

    axios code added

--- 04_useEffect_hook/src/App.jsx
+++ 04_useEffect_hook/src/App.jsx
@@ -1,6 +1,6 @@
 import LifeCycle from "./components/01_lifeCycle";
 import Fetch_02 from "./components/02_UseEffect_fetch";
-import Axious from "./components/03_Axious";
+import Axious from "./components/03_Axios";
 
 
 const App=()=>{
